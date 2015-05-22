4e5ddb400e0f1641a86d437029c3b4b877284c81
Jeremy Krieg <Jeremy.Krieg@YourAmigo.com>
2015-05-22 14:13:14 +0930

    Added code to download and install the windows JDK.

---
+++ scripts/setup-wine.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+export WINEDEBUG=-all
+mkdir -p $HOME/.jdk_cache
+echo "Downloading JDK 7 update 72 for Windows"
+wget -N -P $HOME/.jdk_cache --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u72-b14/jdk-7u72-windows-x64.exe
+echo "Installing the JDK into Wine TODO: only install if not already installed"
+wine $HOME/.jdk_cache/jdk-7u72-windows-x64.exe /s STATIC=1 WEB_JAVA=0 
--- scripts/test.sh
+++ scripts/test.sh
@@ -3,4 +3,8 @@
 export WINEDEBUG=-all
 echo "Running the test script"
 echo -e "echo Hi there from wine!\nexit" | wine cmd
+echo -e "\nRunning winejava"
+scripts/winejava -version
+echo -e "\nRunning winemvn"
+scripts/winejava test
 echo -e "\nWas wine installed?"
